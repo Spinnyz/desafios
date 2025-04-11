b29a809a45393e8e5d4a335774274f7450f520fa
pedro <pedrolucasgonza31@gmail.com>
2025-04-10 22:02:44 -0300

    up

---
+++ script.js
@@ -0,0 +1,32 @@
+var fundo = document.querySelector("body");
+var cback = document.querySelector("h1.cback");
+var button = document.querySelector("button.botao");
+
+function mudarcor() {
+    var valor = Math.floor(Math.random() * 9) + 1;
+    if (valor == 1) {
+        fundo.style.backgroundColor = "red";
+    } else if (valor == 2) {
+        fundo.style.backgroundColor = "blue";
+    } else if (valor == 3) {
+        fundo.style.backgroundColor = "yellow";
+    } else if (valor == 4) {
+        fundo.style.backgroundColor = "green";
+    } else if (valor ==6){
+        fundo.style.backgroundColor = "black";
+        button.style.color = "white"
+    } else if (valor == 5){
+        fundo.style.backgroundColor = ("purple");
+    } else if (valor == 7){
+        fundo.style.backgroundColor = ("orange")
+    }else if (valor == 8){
+        fundo.style.backgroundColor = ("pink")
+    }else if (valor == 9){
+        fundo.style.backgroundColor = ("blueviolet")
+    }
+ 
+}
+
+button.addEventListener("click", mudarcor);
+
+
