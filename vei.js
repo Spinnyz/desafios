611b6d2a727f88558c006211ceca68d1ad71f17b
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-18 19:14:08 -0300

    up

--- vei.js
+++ vei.js
@@ -8,7 +8,7 @@ var title =document.querySelector ("h1.titulo")
 if (horario >= 0 && horario < 6) {
     hora.innerHTML = `São ${horario}h - Boa madrugada`;
     cor.style.backgroundColor = "#3E3B35"
-    img.style.backgroundurl
+    img.scr = 
 } else if (horario >= 6 && horario < 12) {
     hora.innerHTML = `São ${horario}h - Bom dia`;
     cor.style.backgroundColor = "#E8A02C"
