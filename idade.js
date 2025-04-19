0dce3e00e10e2626be62749be17402c41ae07b81
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-19 12:11:05 -0300

    up

---
+++ idade.js
@@ -0,0 +1,10 @@
+var ano = new Date().getFullYear();
+var masculino = document.querySelector("input#imas")
+var feminino =document.querySelector ("input#ifem")
+var atualano = document.querySelector ("input#iano")
+
+function analizador(){
+    if (atualano)
+
+
+}
