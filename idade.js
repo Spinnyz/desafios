4c973a77b44ae0c44248e40cd240c203362e7580
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-19 12:22:40 -0300

    up

--- idade.js
+++ idade.js
@@ -2,9 +2,16 @@ var ano = new Date().getFullYear();
 var masculino = document.querySelector("input#imas")
 var feminino =document.querySelector ("input#ifem")
 var atualano = document.querySelector ("input#iano")
+var botão = document.querySelector ("input#iver")
 
 function analizador(){
-    if (atualano)
+    if (atualano.value.length > 4 || atualano.value > ano){
+        alert ("Verifique o ano novamente")
+    } else {
+        alert ("Ano correto")
+    }
 
 
 }
+
+botão.addEventListener ("click", analizador)
