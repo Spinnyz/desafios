f39796707d5354f3bbcdcf253f73cb534645e03b
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-20 19:18:49 -0300

    up

--- idade.js
+++ idade.js
@@ -3,11 +3,17 @@ var masculino = document.querySelector("input#imas")
 var feminino =document.querySelector ("input#ifem")
 var atualano = document.querySelector ("input#iano")
 var botão = document.querySelector ("input#iver")
+var text =document.querySelector ("p.texto")
+var img = document.querySelector ("p.img")
 
 function analizador(){
-    if (Number(atualano.value) > ano || atualano.value.length < 4 || atualano.value.length > 4 || Number(atualano.value) < 1908 || atualano.value.length == 0) {
+    if (Number(atualano.value) > ano || atualano.value.length < 4 || atualano.value.length > 4 || Number(atualano.value) < 1908) {
         alert ("Ano inválido")
-    }else if ()
+    }else {
+        text.innerHTML = `Você nasceu em ${Number(atualano.value)} então você tem ${ano - atualano.value} anos de idade }`
+        
+
+    }
 
 
 }
