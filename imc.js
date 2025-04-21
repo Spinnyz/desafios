e5766c1b78da38e0bd75d9b1af61604c907ab259
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-20 21:14:23 -0300

    up

--- imc.js
+++ imc.js
@@ -3,7 +3,9 @@ var altura = document.querySelector ("input#ialtura")
 var peso =document.querySelector ("input#ipeso")
 
 function calcular(){
-   var resultado = Number(altura.value)**2 / Number(peso.value)
-   document.write (resultado)
+   var alt = Number(altura.value)**2
+   var numberpeso = Number(peso.value)
+   var result = alt / number peso  
+   document.write (result)
 
 }
