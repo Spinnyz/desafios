40f6d64808aec105280060ef89ea182ca55c8bb0
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-20 21:26:00 -0300

    up

--- imc.js
+++ imc.js
@@ -1,11 +1,26 @@
 var nome = document.querySelector ("input#iname")
 var altura = document.querySelector ("input#ialtura")
 var peso =document.querySelector ("input#ipeso")
+var texto =document.querySelector ("p.texta")
+var botao =document.querySelector ("button#calcular")
 
 function calcular(){
-   var alt = Number(altura.value)**2
-   var numberpeso = Number(peso.value)
-   var result = alt / number peso  
-   document.write (result)
-
+    var alturaValor = Number(altura.value);
+    var pesoValor = Number(peso.value);
+    var imc = pesoValor / (alturaValor ** 2);
+    if (imc < 18.5) {
+        texto.innerHTML = `Seu imc é de ${imc}`
+      } else if (imc < 25) {
+        texto.innerHTML = `Seu imc é de ${imc}`
+      } else if (imc < 30) {
+        texto.innerHTML = `Seu imc é de ${imc}`
+      } else if (imc < 35) {
+        texto.innerHTML = `Seu imc é de ${imc}`
+      } else if (imc < 40) {
+        texto.innerHTML = `Seu imc é de ${imc}`
+      } else {
+        texto.innerHTML = `Seu imc é de ${imc}`
+    }
 }
+
+botao.addEventListener ("click", calcular)
