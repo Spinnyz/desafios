361cdde0f785d3b814a074aa768ff310fa63b2ef
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-21 21:30:21 -0300

    yp

--- lampada.js
+++ lampada.js
@@ -3,8 +3,10 @@ var desligar = document.querySelector (".butaod")
 var lampada = document.querySelector ("#lampada")
 
 function ligar(){
+    lampada.src = "buscape-logo-8 (1).png"
    
 }
+
 
 
 desligar.addEventListener ("click",ligdes)
