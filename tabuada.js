ce02a6912935d5846fadd61c930c0c62a48023c7
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-24 20:32:33 -0300

    up

---
+++ tabuada.js
@@ -0,0 +1,8 @@
+var valor  = document.querySelector ("input#ivalor")
+var gerar = document.querySelector ("input#gerar")
+
+function calcular(){
+    if (valor.ariaValueMax.length ==0 ){
+        alert ("Digite um valor")
+    }
+}
