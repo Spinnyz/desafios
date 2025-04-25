31cd83cc7a869c78e7eb1a635701ae9b00fb22a2
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-24 21:04:47 -0300

    up

--- tabuada.js
+++ tabuada.js
@@ -1,9 +1,21 @@
 var valor  = document.querySelector ("input#ivalor")
-var gerar = document.querySelector ("input#gerar")
-
+var gerar = document.querySelector ("button#gerar")
+var texto = document.querySelector ("select#itab")
 function calcular(){
-    if (valor.ariaValueMax.length ==0 ){
+    if (valor.value.length == 0  ){
         alert ("Digite um valor")
+    }else{
+        var v = Number(valor.value)
+        var c = 1
+        while (c <= 10){
+            texto.innerHTML =""
+            var op = document.createElement ("option")
+            var vaop = document.setAttribute ("value","valor 1")
+            texto.innerHTML += `${v} x ${c} = ${v*c} <br>`
+            c++
+            texto.appendChild (op)
+            op.value = `tab${c}`; 
+        }
     }
 }
 
