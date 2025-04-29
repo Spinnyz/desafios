0d38d90927cfbdd9480b5a871c4e442005a1c23b
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-29 19:30:47 -0300

    up

--- tabuada.js
+++ tabuada.js
@@ -3,5 +3,9 @@ var calculart = document.querySelector ("input#btnCalcular")
 var select = document.querySelector ("select#operacao")
 
 function calcular(){
-
+    if(inicio.value.length == 0){
+        alert ("insira um valor")
+    }else {
+        
+    }
 }
