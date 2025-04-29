cabb5611211d275bd2cf9ea4d49eebf5c18c7dbb
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-29 19:37:17 -0300

    up

--- tabuada.js
+++ tabuada.js
@@ -1,5 +1,5 @@
 var inicio = document.querySelector ("input#ivalor")
-var calculart = document.querySelector ("input#btnCalcular")
+var calculart = document.querySelector ("button#btnCalcular")
 var select = document.querySelector ("select#operacao")
 
 function calcular(){
@@ -7,6 +7,12 @@ function calcular(){
     if(inicio.value.length == 0){
         alert ("insira um valor")
     }else {
-
+        var i = Number(inicio.value)
+        var c = 10  
+        do{
+            select.innerHTML = `${i} X ${c} = ${i*c}`
+        }while (c <=10)
     }
 }
+
+calculart.addEventListener ("click",calcular)
