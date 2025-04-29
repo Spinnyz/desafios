b35753062f283629e4e05803cfa4d112c932c557
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-04-29 20:47:39 -0300

    up

--- tabuada.js
+++ tabuada.js
@@ -3,18 +3,19 @@ var calculart = document.querySelector ("button#btnCalcular")
 var select = document.querySelector ("#ioperacao")
 
 function calcular(){
-    inicio.innerHTML =" "
     if(inicio.value.length == 0){
         alert ("insira um valor")
     }else {
         var i = Number(inicio.value)
         var c = 1
+
         do{
+            inicio.innerHTML =""
             let item = document.createElement("option");
-            select.innerHTML = `${i} X ${c} = ${i*c}`
+            item.text = `${i} X ${c} = ${i*c}`
             select.appendChild(item);
             c++
-        }while (c <=10)
+        }while (c <=999)
     }
 }
 
